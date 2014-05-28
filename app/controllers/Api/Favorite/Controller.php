1ce678589eb6c8ce371cb0ec9da34686a4941685
nikolai sokolov <nslv84@gmail.com>
2014-05-28 16:19:56 +0300

    update favorite

--- app/controllers/Api/Favorite/Controller.php
+++ app/controllers/Api/Favorite/Controller.php
@@ -44,8 +44,11 @@ class Controller extends BaseController {
 
         foreach($data as $key => $row){
             $data[$key]['data'] = json_decode($row['data']);
-            $data[$key]['data']->organization = json_decode($row['organization']);
-            unset($data[$key]['organization']);
+            
+            if ($role === BaseDataModel::JOB) {
+                $data[$key]['data']->organization = json_decode($row['organization']);
+                unset($data[$key]['organization']);
+            }
         }
 
         return new RestResponse($data);
