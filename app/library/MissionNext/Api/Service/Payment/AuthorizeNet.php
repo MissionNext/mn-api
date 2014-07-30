bb7f3a5506f681af1faaa8d8d0ff2e553e2cfc15
Angelys <angelys10@gmail.com>
2014-07-30 15:06:21 +0300

    Subscription name added

--- app/library/MissionNext/Api/Service/Payment/AuthorizeNet.php
+++ app/library/MissionNext/Api/Service/Payment/AuthorizeNet.php
@@ -167,6 +167,7 @@ class AuthorizeNet extends AbstractPaymentGateway implements ISecurityContextAwa
         $sub->billToCity = $data['required_data']['city'];
         $sub->customerPhoneNumber = $data['required_data']['phone'];
 
+        $sub->name = "MissionNext network subscription";
         $sub->orderDescription = $this->addAppsToPayment($data['subscriptions'], $user->role(), $data['period']);
 
         $sub->customerId = $user['id'];
