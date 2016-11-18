7c0be0feb55334f3f5c9ee2031650c9755e5bad4
Vladislav Yaschuk <crusader@masterofcode.com>
2016-11-18 13:31:15 +0200

    Changed credentials for authorize.net

--- app/library/MissionNext/Provider/PaymentProvider.php
+++ app/library/MissionNext/Provider/PaymentProvider.php
@@ -15,7 +15,8 @@ class PaymentProvider extends ServiceProvider
         $this->app->bind(PaymentGatewayInterface::class, function(Application $app)
         {
 
-            return (new SecurityContextResolver( new AuthorizeNet( new \AuthorizeNetAIM('6W9w2XnmkkRj','938c4CBqpj84w9Cs'), new \AuthorizeNetARB('6W9w2XnmkkRj','938c4CBqpj84w9Cs'), $app )))->getResolvedObject();
+            // return (new SecurityContextResolver( new AuthorizeNet( new \AuthorizeNetAIM('6W9w2XnmkkRj','938c4CBqpj84w9Cs'), new \AuthorizeNetARB('6W9w2XnmkkRj','938c4CBqpj84w9Cs'), $app )))->getResolvedObject();
+            return (new SecurityContextResolver( new AuthorizeNet( new \AuthorizeNetAIM('7b5t92TM3tW','9G6Q89y5es8fP7WC'), new \AuthorizeNetARB('7b5t92TM3tW','9G6Q89y5es8fP7WC'), $app )))->getResolvedObject();
         });
     }
 } 
