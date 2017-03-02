401e37e9e962f9d7d49b93d2610d01fdffaf64a2
Crusader <crusader@masterofcode.com>
2017-03-02 12:43:57 +0200

    Update from server.

--- app/views/admin/mail/user/status.blade.php
+++ app/views/admin/mail/user/status.blade.php
@@ -1,6 +1,28 @@
 
-Dear {{ ucfirst($user['username']) }},
-Your global MissionNext network status changed
-Current status: {{{ $user['is_active'] ? 'Access Granted.' : 'Access Denied.'  }}}
+Dear {{ $user['username'] }},
+Your MissionNext network status changed
+Current status is now: {{{ $user['is_active'] ? 'Access Granted' : 'Access Denied'  }}}
+
+IF REGISTERED AS AN EXPLORENEXT AGENCY OR TEACHNEXT SCHOOL: 
+   
+We are pleased to inform you that your request for partnership with MissionNext has been approved!  You will soon have access to our online database of potential candidates to fill your open positions.    
+   
+Please log in to your account from https://new.missionnext.org/welcome/login-here/ (after choosing the appropriate service) with your username and password to make your activation payment. Once your payment is received, you will receive instructions on how to set up your profile and post your jobs in order to make the most of your MissionNext partnership.   
+
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+IF REGISTERED AS AN EXPLORENEXT AGENCY REP OR TEACHNEXT AGENCY TO SERVICE SCHOOLS:  
+      
+We are pleased to inform you that your MissionNext Rep Registration has been approved!  You will soon be able to affiliate with your mission agency and/or schools to view their posted jobs and view candidate profiles.    
+   
+Please watch for an email from MissionNext with a coupon code that will allow you to activate your account without payment.  
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+TO ALL:
+    
+If you have any additional questions at this time, please contact us at headquarters@missionnext.org   
+   
+We look forward to partnering with you!   
+   
+MissionNext Partner Support Team  
 Thank you,
 MissionNext Team
