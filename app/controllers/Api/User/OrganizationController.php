332717e9c25b605c17da81166d735aa92cd75cbd
Crusader <crusader@masterofcode.com>
2017-06-14 14:55:12 +0300

    Bug #61635. Fix organization names in the inquiries list for agency.

--- app/controllers/Api/User/OrganizationController.php
+++ app/controllers/Api/User/OrganizationController.php
@@ -3,6 +3,8 @@
 
 namespace MissionNext\Controllers\Api\User;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Request;
 use MissionNext\Api\Response\RestResponse;
 use MissionNext\Controllers\Api\BaseController;
 use MissionNext\Models\User\User;
@@ -29,5 +31,26 @@ class OrganizationController extends BaseController
         $user =  $userRepo->find($userId);
 
         return new RestResponse( $userRepo->organizationJobsForUser($organization, $user) );
+    }
+
+    public function getOrganizationNames()
+    {
+        $request = Request::instance();
+        $org_ids = $request->query->get('organizations');
+        $organizations = DB::table('organization_cached_profile')
+            ->select(DB::raw("id, data->'profileData'->>'organization_name' as organization_name, data->'profileData'->>'first_name' as first_name, data->'profileData'->>'last_name' as last_name"))
+            ->whereIn('id', $org_ids)
+            ->get();
+
+        $org_names = [];
+        foreach ($organizations as $org) {
+            if (!empty($org->organization_name)) {
+                $org_names[$org->id] = $org->organization_name;
+            } else {
+                $org_names[$org->id] = $org->first_name." ".$org->last_name;
+            }
+        }
+
+        return new RestResponse($org_names);
     }
 } 
