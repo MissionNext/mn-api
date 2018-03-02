50d95d1be5e967cab9526e7b8e24c08eb1ff87bc
sirCrusader <sercrusader@gmail.com>
2018-03-02 12:41:11 +0200

    Get params from the config file for connection.

--- app/library/MissionNext/Provider/PaymentProvider.php
+++ app/library/MissionNext/Provider/PaymentProvider.php
@@ -7,14 +7,18 @@ use Illuminate\Support\ServiceProvider;
 use MissionNext\Api\Auth\SecurityContextResolver;
 use MissionNext\Api\Service\Payment\AuthorizeNet;
 use MissionNext\Api\Service\Payment\PaymentGatewayInterface;
+use Illuminate\Support\Facades\Config;
 
 class PaymentProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind(PaymentGatewayInterface::class, function(Application $app)
+        $api_id = Config::get('app.api_login_id');
+        $key = Config::get('app.transaction_key');
+
+        $this->app->bind(PaymentGatewayInterface::class, function(Application $app, $api_id, $key)
         {
-            return (new SecurityContextResolver( new AuthorizeNet( new \AuthorizeNetAIM(config('app.api_login_id'), config('app.transaction_key')), new \AuthorizeNetARB(config('app.api_login_id'), config('app.transaction_key')), $app )))->getResolvedObject();
+            return (new SecurityContextResolver( new AuthorizeNet( new \AuthorizeNetAIM($api_id, $key), new \AuthorizeNetARB($api_id, $key), $app )))->getResolvedObject();
         });
     }
 } 
