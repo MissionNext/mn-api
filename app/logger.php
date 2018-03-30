35dabecc0309aa014745347687e27ce2ced0d50a
sirCrusader <sercrusader@gmail.com>
2018-03-30 10:39:52 +0300

    Change permissions of the log-files.

--- app/logger.php
+++ app/logger.php
@@ -6,7 +6,7 @@ use Monolog\Handler\StreamHandler;
 DB::listen(function($sql, $bindings, $time){
     $logFile = storage_path('logs/custom_logs/query-'.date('Y-m-d H').'.txt');
     $monolog = new Logger('log');
-    $monolog->pushHandler(new StreamHandler($logFile), Logger::INFO);
+    $monolog->pushHandler(new StreamHandler($logFile, Logger::DEBUG, true, 0775), Logger::INFO);
     $monolog->info('-------------------------------------------');
     $monolog->info($sql, compact('bindings', 'time'));
 });
